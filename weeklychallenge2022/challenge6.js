04c15ce158d60a8ad5e30b73b6a90dabdbc04040
chuli99 <juliancastillo0399@gmail.com>
2023-02-21 19:01:18 -0300

    challenge #6 added

--- weeklychallenge2022/challenge6.js
+++ weeklychallenge2022/challenge6.js
@@ -6,6 +6,13 @@
  * Enunciado: Crea un programa que invierta el orden de una cadena de texto sin usar funciones propias del lenguaje que lo hagan de forma automática.
  * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
  */
+
 function inverseString(string){
-    
+    let newString = "";
+    for (let i = string.length-1; i >= 0; i--){
+        newString += string[i];
+    }
+    return newString;
 }
+
+console.log(inverseString("Hola mundo"));
